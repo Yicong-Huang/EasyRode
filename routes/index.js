d308afa8aaae1a8c53744caa537c6c88783d43a3
Yicong-Huang <hyc541978023@gmail.com>
2018-02-02 23:35:44 -0800

    added search

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+router.get('/search', function(req, res, next) {
+    res.render('packages', { title: 'Packages' });
+});
+
+
 
 module.exports = router;
